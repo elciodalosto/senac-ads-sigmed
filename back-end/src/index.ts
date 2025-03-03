3dd806445ef1b21acb57b047c0b07165968f0065
Elcio Augusto Dalosto <edalosto@gmail.com>
2025-03-03 17:01:12 -0300

    adição do cors no back

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,40 +1,42 @@
-import express, { Request, Response } from "express"
-import { PrismaClient } from "@prisma/client"
-import router from "./routes/index"
-import dotenv from "dotenv"
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import router from "./routes/index";
+import dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
+const app = express();
 
-export const prisma = new PrismaClient()
-
-const app = express()
-const port = process.env.API_PORT || 9090
+// Configurar CORS
+app.use(cors());
+dotenv.config();
+export const prisma = new PrismaClient();
+const port = process.env.API_PORT || 9090;
 
 async function main() {
-  app.use(express.json())
+  app.use(express.json());
 
-  app.use(router)
+  app.use(router);
 
   app.get("/health", (req, res) => {
-    res.send("API SIGMED is running 🚀")
-  })
+    res.send("API SIGMED is running 🚀");
+  });
 
   app.all("*", (req: Request, res: Response) => {
-    res.status(404).json({ error: `Route ${req.originalUrl} not found` })
-  })
+    res.status(404).json({ error: `Route ${req.originalUrl} not found` });
+  });
 
   app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`)
-  })
+    console.log(`Server is listening on port ${port}`);
+  });
 }
 
 main()
   .then(async () => {
-    await prisma.$connect()
-    console.log("Connected to the MySQL database 🚀")
+    await prisma.$connect();
+    console.log("Connected to the MySQL database 🚀");
   })
-  .catch(async e => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
