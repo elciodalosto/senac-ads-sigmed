4cec28b17a79d4ab20ed5b70ed7d2cdf21cf8c51
Elcio Augusto Dalosto <edalosto@gmail.com>
2025-03-03 17:01:20 -0300

    alteração de rotas

--- front-end/app/pages/login.tsx
+++ front-end/app/pages/login.tsx
@@ -1,19 +1,38 @@
+import { ThemedText } from "@/components/ThemedText";
+import { Link } from "expo-router";
 import { useRouter } from "expo-router";
 import { View, Text, Button, StyleSheet } from "react-native";
 
 export default function Login() {
   const router = useRouter();
 
-  const handleNavegar = () => {
-    router.push("/home");
-  };
+  // const handleNavegar = () => {
+  //   router.push("/menu");
+  // };
 
   return (
     <View>
       <Text>Faça seu login</Text>
-      <Button title="Entrar" onPress={handleNavegar} />
+      <Link href={"/pages/menu"} style={styles.login}>
+        <ThemedText type="link">LOGIN</ThemedText>
+      </Link>
+
+      {/* <Button title="Entrar" onPress={handleNavegar} /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  login: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    width: "80%",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    borderRadius: 15,
+    backgroundColor: "#64FCD9",
+    color: "#000",
+    fontWeight: "bold",
+  },
+});
