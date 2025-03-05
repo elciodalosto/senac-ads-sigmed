f2bd6aa0a01bfddc7f97345a6b13d076b1c5daa6
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-05 02:42:16 -0300

    feat: Implement inventory management page with search functionality and improved UI

--- front-end/app/pages/medicineStock.tsx
+++ front-end/app/pages/medicineStock.tsx
@@ -1,29 +1,44 @@
 import { api_sigmed } from "@/api/axios"
-import { Link } from "expo-router"
+import { Link, useRouter } from "expo-router"
 import { useEffect, useState } from "react"
-import { Image, StyleSheet, Text, View } from "react-native"
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native"
+import { Search, Layers, ArrowLeft } from "lucide-react-native"
+import { Router } from "expo-router"
 
-interface User {
+interface InventoryItem {
   id: number
-  name: string
-  email: string
-  password: string
-  role: string
-  createdAt: Date
-  prescriptions?: string[]
-  treatments?: string[]
+  quantity: number
+  medication: {
+    id: number
+    name: string
+    description: string
+    dosageForm: string
+    concentration: string
+    createdAt: string
+  }
 }
 
 export default function Home() {
-  const [users, setUsers] = useState([])
+  const [inventoryItems, setinventoryItems] = useState([])
   const [loading, setLoading] = useState(true)
+  const router = useRouter()
+  const [searchText, setSearchText] = useState("")
 
-  const fetchUsers = async () => {
+  const fetchInventoryItems = async () => {
     setLoading(true)
     try {
-      const response = await api_sigmed.get("/user/getall")
+      console.log("rodei")
+      const response = await api_sigmed.get("/inventory/getall")
       console.log("resultado ->", response.data)
-      setUsers(response.data)
+      setinventoryItems(response.data)
     } catch (error) {
       console.log("Erro:", error)
     } finally {
@@ -32,22 +47,89 @@ export default function Home() {
   }
 
   useEffect(() => {
-    fetchUsers()
+    fetchInventoryItems()
   }, [])
 
+  const filteredItems = inventoryItems.filter((item: InventoryItem) =>
+    item?.medication?.name.toLowerCase().includes(searchText.toLowerCase())
+  )
+
   return (
-    <View>
-      <Text>Estoque de medicamentos</Text>
-      {loading && <Text>Carregando...</Text>}
-      {users.map((user: User) => (
-        <View key={user.id}>
-          <Text>{user.name}</Text>
-          <Text>{user.email}</Text>
-          <Text>{user.role}</Text>
-        </View>
-      ))}
+    <View style={styles.container}>
+      <Text style={styles.title}>Estoque de medicamentos</Text>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Pesquise o medicamento"
+          value={searchText}
+          onChangeText={setSearchText}
+        />
+        <Search size={20} />
+      </View>
+      {loading ? (
+        <Text>Carregando...</Text>
+      ) : (
+        <FlatList
+          data={filteredItems}
+          keyExtractor={item => item.id.toString()}
+          renderItem={({ item }: { item: InventoryItem }) => (
+            <View style={styles.card}>
+              <Layers size={24} />
+              <Text style={styles.medicineName}>{item?.medication?.name}</Text>
+              <Text style={styles.quantity}>Qnt. {item?.quantity || 0}</Text>
+            </View>
+          )}
+        />
+      )}
+
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+        <ArrowLeft size={20} color="white" />
+        <Text style={styles.backButtonText}>Voltar</Text>
+      </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 20, backgroundColor: "#fff" },
+  username: { fontSize: 18, color: "#333", marginBottom: 10 },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    borderRadius: 10,
+    padding: 5,
+    marginBottom: 20
+  },
+  searchInput: { flex: 1, padding: 5 },
+  searchIcon: { marginRight: 5 },
+  card: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderColor: "#e0e0e0",
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 10
+  },
+  icon: { marginRight: 10 },
+  medicineName: { fontSize: 16, flex: 1, paddingLeft: 10 },
+  quantity: { fontSize: 14, color: "#888" },
+  backButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#333",
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 20
+  },
+  backButtonText: { color: "#fff", marginLeft: 5 }
+})
