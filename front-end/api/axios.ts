1a568f7eb3988dc63fab670a346133b942939bb1
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-05 13:02:39 -0300

    feat: Add instructions for finding local IP address in axios configuration

--- front-end/api/axios.ts
+++ front-end/api/axios.ts
@@ -1,5 +1,14 @@
 import Axios from "axios"
-import Config from "react-native-config"
+
+//  Como ver o seu IP?
+
+//  Windows
+//  1 - No CMD, digite: ipconfig
+//  2 - Procure por algo como: Endereço IPv4. . . . . . . . . . . . . : 192.168.x.x
+
+//  Linux
+// 1 - No terminal, digite: ifconfig
+// 2 - Resultado: procure por inet para o IPv4 (ex: inet 192.168.x.x).
 
 // Adicione seu IP 🚨
 export const api_sigmed = Axios.create({
