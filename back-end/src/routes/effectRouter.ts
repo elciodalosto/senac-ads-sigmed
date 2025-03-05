e0a145cd8a3e25450ed47477bb5c96d896b620ce
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-05 00:04:56 -0300

    feat: Update effect controller to include medicationId and description fields

--- back-end/src/routes/effectRouter.ts
+++ back-end/src/routes/effectRouter.ts
@@ -3,10 +3,8 @@ import * as EffectController from "../controllers/effectController";
 
 const effectRouter = express.Router();
 
-// Criar um novo efeito colateral atrelado a um paciente
 effectRouter.post("/create", EffectController.createEffect);
 
-// Buscar todos os efeitos colaterais de um paciente específico
 effectRouter.get("/get/:patientId", EffectController.getEffectsByPatient);
 
 export default effectRouter;
