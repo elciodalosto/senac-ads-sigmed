7e46504a6bcdf5b309c410b65390af0b34ba62e7
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-05 02:43:22 -0300

    feat: Update API configuration to use dynamic port from environment variables

--- front-end/api/axios.ts
+++ front-end/api/axios.ts
@@ -2,5 +2,5 @@ import Axios from "axios"
 import Config from "react-native-config"
 
 export const api_sigmed = Axios.create({
-  baseURL: `http://${Config.IP_LOCAL}:9090`
+  baseURL: `http://${Config.IP_LOCAL}:${Config.API_SIGMED_PORT}`
 })
