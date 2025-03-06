19558097ca63617c86b8a7dd49b4f5a81c34ee87
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-06 01:57:33 -0300

    feat: implement patient search functionality with a dedicated SearchBar component

--- front-end/app/pages/menu.tsx
+++ front-end/app/pages/menu.tsx
@@ -1,14 +1,12 @@
-import { ThemedText } from "@/components/ThemedText";
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemedText } from "@/components/ThemedText"
+import { Link } from "expo-router"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function Menu() {
   return (
     <View style={styles.container}>
-      <Text>MENU PRINCIPAL</Text>
-
       <Link href={"/pages/patientSearch"} style={styles.prosseguir}>
-        <ThemedText type="link">BUSCAR PACIENTES</ThemedText>
+        <ThemedText type="link">PACIENTES</ThemedText>
       </Link>
       <Link href={"/pages/colateralEffects"} style={styles.prosseguir}>
         <ThemedText type="link">EFEITOS COLATERAIS</ThemedText>
@@ -19,16 +17,15 @@ export default function Menu() {
       <Link href={"/pages/settings"} style={styles.prosseguir}>
         <ThemedText type="link">CONFIGURAÇÕES</ThemedText>
       </Link>
-
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   prosseguir: {
     paddingVertical: 10,
@@ -42,5 +39,5 @@ const styles = StyleSheet.create({
     color: "#000",
     fontWeight: "bold",
     marginBottom: 15
-  },
-});
+  }
+})
