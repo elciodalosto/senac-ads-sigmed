fd788547ca81bc2a95362cc65c6d367f4c3fb19c
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-06 01:57:18 -0300

    feat: rename components for clarity and consistency

--- front-end/app/pages/colateralEffects.tsx
+++ front-end/app/pages/colateralEffects.tsx
@@ -1,12 +1,12 @@
-import { Link } from "expo-router";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router"
+import { Image, StyleSheet, Text, View } from "react-native"
 
-export default function Home() {
+export default function ColateralEffects() {
   return (
     <View>
       <Text>EFEITOS COLATERAIS NO PACIENTE</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
