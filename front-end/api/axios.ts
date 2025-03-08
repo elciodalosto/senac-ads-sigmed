764ce475a093bc89db7eddf65e795f155162a2fd
Giulyano Santos <giulyanofelipesantos@gmail.com>
2025-03-08 14:39:30 -0300

    feat: add environment configuration and update axios base URL to use environment variables

--- front-end/api/axios.ts
+++ front-end/api/axios.ts
@@ -1,16 +1,6 @@
 import Axios from "axios"
+import { SIGMED_API_URL, SIGMED_API_PORT } from "@env"
 
-//  Como ver o seu IP?
-
-//  Windows
-//  1 - No CMD, digite: ipconfig
-//  2 - Procure por algo como: Endereço IPv4. . . . . . . . . . . . . : 192.168.x.x
-
-//  Linux
-// 1 - No terminal, digite: ifconfig
-// 2 - Resultado: procure por inet para o IPv4 (ex: inet 192.168.x.x).
-
-// Adicione seu IP 🚨
 export const api_sigmed = Axios.create({
-  baseURL: `http://000.00.00.00:9090`
+  baseURL: `${SIGMED_API_URL}:${SIGMED_API_PORT}`
 })
